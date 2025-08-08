07fdc7d52b9eb928bc772e59e3e03713ec8cae51
nishita-morjariya <nishitamorjariya@gmail.com>
2025-08-08 11:48:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-    base:'/jetking/'
+    tailwindcss()
   ],
+  base:'/jetking/'
 })
